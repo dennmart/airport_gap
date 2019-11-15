15e9e8bf92c6311ca7d32512ac622f4de6b70bf7
Dennis Martinez <dennis@dennmart.com>
2019-11-15 17:36:26 +0900

    Set up seeds with additional data for new models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require "csv"
 
-puts "Importing Airport data..."
+puts "=========================================="
+puts "Importing airport data..."
+Airport.delete_all
 CSV.foreach(Rails.root.join("db", "airports.csv"), headers: true) do |row|
   next if row["iata"].blank?
 
@@ -16,3 +18,22 @@ CSV.foreach(Rails.root.join("db", "airports.csv"), headers: true) do |row|
     timezone: row["timezone"]
   )
 end
+puts "Done importing airport data!"
+puts "==========================================\n\n"
+
+puts "=========================================="
+puts "Creating user..."
+user = User.find_or_create_by(email: "dennis@dev-tester.com")
+puts "Done creating user!\n"
+puts "Email: #{user.email}"
+puts "Token: #{user.token}"
+puts "==========================================\n\n"
+
+puts "=========================================="
+puts "Creating favorites for user..."
+Favorite.delete_all
+Airport.order("RANDOM()").limit(10).each do |airport|
+  Favorite.create(user: user, airport: airport)
+end
+puts "Done creating favorites!"
+puts "==========================================\n\n"
