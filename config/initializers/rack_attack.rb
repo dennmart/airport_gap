698dddcadec14d060534bfa780e8aa22858cb198
Dennis Martinez <dennis@dennmart.com>
2019-12-03 16:03:36 +0900

    Fix rate limiting endpoint and bump up to 100 reqs/min.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
-# Throttle public endpoints to 10 requests per minute.
-Rack::Attack.throttle("requests by ip", limit: 10, period: 60) do |request|
-  if request.path.start_with?("/airports")
+# Throttle all API endpoints to 100 requests per minute.
+Rack::Attack.throttle("requests by IP address", limit: 100, period: 60) do |request|
+  if request.path.start_with?("/api")
     request.ip
   end
 end
