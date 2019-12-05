9866a75eb11f3eb91a68e1b725e87c297667d69f
Dennis Martinez <dennis@dennmart.com>
2019-12-05 10:19:17 +0900

    No need to create example user in seeds anymore

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,23 +20,3 @@ CSV.foreach(Rails.root.join("db", "airports.csv"), headers: true) do |row|
 end
 puts "Done importing airport data!"
 puts "==========================================\n\n"
-
-puts "=========================================="
-puts "Creating user..."
-user = User.find_or_create_by(email: "dennis@dev-tester.com") do |user|
-  user.password = "airport-gap-123"
-end
-puts "Done creating user!\n"
-puts "Email: #{user.email}"
-puts "Password: airport-gap-123"
-puts "Token: #{user.token}"
-puts "==========================================\n\n"
-
-puts "=========================================="
-puts "Creating favorites for user..."
-Favorite.delete_all
-Airport.order("RANDOM()").limit(10).each do |airport|
-  Favorite.create(user: user, airport: airport)
-end
-puts "Done creating favorites!"
-puts "==========================================\n\n"
