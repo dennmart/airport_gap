b6e7b7e2a88e98b691f1c5d8cbc6d7beaad93845
Dennis Martinez <dennis@dennmart.com>
2019-12-13 15:39:24 +0900

    Revert "No need to create example user in seeds anymore"
    This reverts commit 9866a75eb11f3eb91a68e1b725e87c297667d69f.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,23 @@ CSV.foreach(Rails.root.join("db", "airports.csv"), headers: true) do |row|
 end
 puts "Done importing airport data!"
 puts "==========================================\n\n"
+
+puts "=========================================="
+puts "Creating example user..."
+user = User.find_or_create_by(email: "airportgap@dev-tester.com") do |user|
+  user.password = "airportgap123"
+end
+puts "Done creating example user!\n"
+puts "Email: #{user.email}"
+puts "Password: airport-gap-123"
+puts "Token: #{user.token}"
+puts "==========================================\n\n"
+
+puts "=========================================="
+puts "Creating 10 favorites for example user..."
+Favorite.delete_all
+Airport.order("RANDOM()").limit(10).each do |airport|
+  Favorite.create(user: user, airport: airport)
+end
+puts "Done creating favorites!"
+puts "==========================================\n\n"
