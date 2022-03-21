698e008eea762aae3f64fd5125ca9042c2415501
Dennis Martinez <dennis@dennmart.com>
2022-03-21 18:00:36 +0900

    Put tailwind.config.js in the root of the repo

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ var tailwindcss = require("tailwindcss");
 
 module.exports = {
   plugins: [
-    tailwindcss('./app/javascript/stylesheets/tailwind.config.js'),
+    tailwindcss('./tailwind.config.js'),
     require("postcss-import"),
     require("postcss-flexbugs-fixes"),
     require("postcss-preset-env")({
