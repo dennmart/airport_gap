dbcd824286e9e94411d2f1cc58fff71047efb311
Dennis Martinez <dennis@dennmart.com>
2024-07-06 17:48:52 +0900

    Set up special testing secret key to bypass rate limiting

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,3 +1,8 @@
+Rack::Attack.safelist('allow any requests for testing purposes') do |request|
+  # Requests are allowed if the return value is truthy
+  request.env['HTTP_TESTING_SECRET'] == Rails.application.credentials.dig(:testing, :secret_key)
+end
+
 # Throttle all API endpoints to 100 requests per minute.
 Rack::Attack.throttle('requests by IP address', limit: 100, period: 60) do |request|
   request.ip if request.path.start_with?('/api')
