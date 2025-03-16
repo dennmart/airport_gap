9e0bbc8a909b95bcc008bfc5c0175bfa7ae3e27e
Dennis Martinez <dennis@dennmart.com>
2025-03-16 16:48:27 +0900

    Remove Tailwind and PostCSS from JS dependencies

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-var tailwindcss = require("tailwindcss");
-
-module.exports = {
-  plugins: [
-    require("@tailwindcss/postcss"),
-    require("@tailwindcss/nesting"),
-    tailwindcss("./tailwind.config.js"),
-  ],
-};
