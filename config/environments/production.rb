7425413dc5ee4391108f76d345dd9ddabaaace27
Dennis Martinez <dennis@dennmart.com>
2026-01-04 16:25:40 +0900

    Allow disabling cache store in production via environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,13 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', nil) }
+  # Replace the default in-process memory cache store with a durable alternative
+  # unless the DISABLE_CACHE_STORE environment variable is set.
+  config.cache_store = if ENV['DISABLE_CACHE_STORE'].present?
+                         :null_store
+                       else
+                         [:redis_cache_store, { url: ENV.fetch('REDIS_URL', nil) }]
+                       end
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :resque
